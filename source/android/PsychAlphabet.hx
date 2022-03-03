fd808b26d00307d57406a6dc785a3c3eb12c0da3
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-03 19:38:39 +0200

    Update PsychAlphabet.hx

--- source/android/PsychAlphabet.hx
+++ source/android/PsychAlphabet.hx
@@ -328,7 +328,7 @@ class PsychAlphabet extends FlxSpriteGroup
 		{
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
-			var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
+			var lerpVal:Float = boundTo(elapsed * 9.6, 0, 1);
 			y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
 			if(forceX != Math.NEGATIVE_INFINITY) {
 				x = forceX;
@@ -346,6 +346,10 @@ class PsychAlphabet extends FlxSpriteGroup
 			typeTimer.destroy();
 		}
 		typeTimer = null;
+	}
+
+	inline public static function boundTo(value:Float, min:Float, max:Float):Float {
+		return Math.max(min, Math.min(max, value));
 	}
 }
 
